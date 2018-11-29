e72fffe6bab48b59d28462f5e7f73bf10aa5358f
Pawel Kucmus <pkucmus@gmail.com>
2018-11-29 19:22:37 +0100

    Increece TMC current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1099,13 +1099,13 @@
   #define HOLD_MULTIPLIER    0.2  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_CURRENT         1000  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
-  #define Y_CURRENT          800
+  #define Y_CURRENT         1000
   #define Y_MICROSTEPS        16
 
-  #define Z_CURRENT          800
+  #define Z_CURRENT         1000
   #define Z_MICROSTEPS        16
 
   #define X2_CURRENT         800
@@ -1117,7 +1117,7 @@
   #define Z2_CURRENT         800
   #define Z2_MICROSTEPS       16
 
-  #define E0_CURRENT         800
+  #define E0_CURRENT        1000
   #define E0_MICROSTEPS       16
 
   #define E1_CURRENT         800
